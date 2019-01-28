ef12166d5f0e7ae3046635f7345ca90ce3691ded
WMXPY <wm@mengw.io>
2019-01-27 23:04:34 -0600

    add enablefor group mode

--- src/group.ts
+++ src/group.ts
@@ -9,9 +9,12 @@ import * as React from "react";
 import { withBrontosaurus } from "./connect";
 import { BrontosaurusEnabledForProps, BrontosaurusProps, WithAuthComponent } from "./declare";
 
+export type EnableForGroupMode = 'oneOf' | 'all';
+
 export type EnableForGroupProp = {
 
     readonly group: string[];
+    readonly mode?: EnableForGroupMode;
     readonly placeholder?: any;
 
     readonly children?: any;
@@ -19,33 +22,38 @@ export type EnableForGroupProp = {
 
 export const EnableForGroupBase: React.ComponentType<EnableForGroupProp> = (props: EnableForGroupProp) => {
 
+    const mode: EnableForGroupMode = props.mode || 'oneOf';
+
+    const token: Token | null = props.visit
+        ? props.auth.visit()
+        : props.auth.strict();
+
+    if (token && token.groups) {
+
+        const groups: string[] = token.groups;
+
+        const valid: boolean = (() => {
+            switch (mode) {
+                case 'all':
+                    return props.group.reduce((previous: boolean, group: string) => {
+                        if (!previous) return false;
+                        return groups.includes(group);
+                    }, true);
+                case 'oneOf':
+                default:
+                    return props.group.some((value: string) => groups.includes(value));
+            }
+        })();
+
+        return valid ? props.children : (props.placeholder || null);
+    }
+
     if (props.visit) {
 
-        const token: Token | null = props.auth.visit();
-
-        if (token && token.groups) {
-
-            const groups: string[] = token.groups;
-            const valid: boolean = props.group.some((value: string) => groups.includes(value));
-
-            return valid ? props.children : (props.placeholder || null);
-        }
-
         return props.placeholder || null;
-    } else {
-
-        const token: Token = props.auth.strict();
-
-        if (token && token.groups) {
-
-            const groups: string[] = token.groups;
-            const valid: boolean = props.group.some((value: string) => groups.includes(value));
-
-            return valid ? props.children : (props.placeholder || null);
-        }
-
-        throw new Error('[Brontosaurus-React] Invalid Token');
     }
+
+    throw new Error('[Brontosaurus-React] Invalid Token');
 };
 
 export const EnableForGroup: WithAuthComponent<EnableForGroupProp> = withBrontosaurus(EnableForGroupBase);
